08f0a3e225e11d8dee85e99c97087a77d6abe771
Göksel Köksal <goksel.koksal@outlook.com>
2015-12-24 19:01:44 +0200

    Initial Commit

---
+++ RxMiddleEarth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxMiddleEarth
+//
+//  Created by Göksel Köksal on 24/12/15.
+//  Copyright © 2015 GK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
